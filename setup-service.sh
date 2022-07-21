84a050e6377ffc83dd62e9b3378ec8839beeccf7
sod-lol <ddssee5511@gmail.com>
2022-07-21 16:19:05 +0430

    Add kill and clean section to setup script

--- setup-service.sh
+++ setup-service.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-cp ./ptools-daemon.service /etc/systemd/system
-
-systemctl daemon-reload
+if [ "$1" = 'k' ];then
+  echo "Killing ptools daemon"
+  systemctl disable ptools-daemon.service
+  systemctl kill -s SIGKILL ptools-daemon.service
+  rm -rf /etc/systemd/system/ptools-daemon.service
+  systemctl daemon-reload
+else
+  echo "Setup ptools daemon"
+  cp ./ptools-daemon.service /etc/systemd/system
+  systemctl daemon-reload
+  systemctl enable ptools-daemon.service
+  systemctl start ptools-daemon.service
+fi
