3577ae35d1c07ed946b664b9d66e17656b99df55
sod-lol <ddssee5511@gmail.com>
2022-08-08 11:58:26 +0430

    Update plot file

--- plot_file_distance.py
+++ plot_file_distance.py
@@ -48,27 +48,69 @@ def plot_throughput(algo, distances, files_size, throughputs):
         plot(fig, ax, f"{algo}_throughput", files_size, distances, throughputs, "File Size[MB]", "Distance [m]", "Throughput [Mb/s]")
 
 def plot_algo(algo, results):
-        thr_fig, thr_ax = plt.subplots(2, 2, figsize=(10,10), subplot_kw=dict(projection='3d'))
-        
+        download_time = []
+        throughput = []
+        distances = []
+        file_size = []
+
+        for specific_algo, folders in results.items():
+                for folder in folders:
+                    iperf_data = extract_iperf_data(folder)
+                    download_time.append(iperf_data["time"])
+                    throughput.append(iperf_data["throughput"])
+                    file_size.append(iperf_data["file_size"])          
+                    groups = re.search('([a-zA-Z]*)_([a-zA-Z]*)_([a-zA-Z]*)_([a-zA-Z0-9]*)_([a-zA-Z0-9]*)', folder)
+                    distances.append(groups.group(5).split('m')[0])
+
+        throughputs = [float(x) for x in throughput]
+        downloads_time = [float(x) for x in download_time]
+        files_size = [float(x) for x in file_size]
+        distances = [float(x) for x in distances]
+
+        all_dist = []
+        for dist in distances:
+                if not dist in all_dist:        
+                        all_dist.append(dist)
+
+        all_fz = []
+        for fz in files_size:
+                if not fz in all_fz:
+                        all_fz.append(fz)
+
+
+        thr_fig, thr_ax = plt.subplots(2, 2, figsize=(10, 10), subplot_kw=dict(projection='3d'))
+
         for i in range(4):
                 thr_ax[i // 2, i % 2].azim = -40
                 thr_ax[i // 2, i % 2].dist = 10
                 thr_ax[i // 2, i % 2].elev = 20
+                thr_ax[i // 2, i % 2].set_xticks(all_fz)
+                thr_ax[i // 2, i % 2].set_yticks(all_dist)
                 thr_ax[i // 2, i % 2].set(xlabel="File Size[MB]", ylabel="Distance [m]", zlabel="Throughput [Mb/s]",
                            title=f"{algo} Throughput")
 
-        dt_fig = plt.figure(clear=True)
-        dt_ax = dt_fig.add_subplot(2, 2, 1, projection='3d')
-        dt_ax.azim = -40
-        dt_ax.dist = 10
-        dt_ax.elev = 20
-        dt_ax.invert_xaxis()
-        dt_ax.set(xlabel="File Size[MB]", ylabel="Distance [m]", zlabel="Download time [s]",
-                      title=f"{algo} Download Time")
+        dt_fig, dt_ax = plt.subplots(2, 2, figsize=(10, 10), subplot_kw=dict(projection='3d'))
+ 
+        for i in range(4):
+                dt_ax[i // 2, i % 2].azim = -40
+                dt_ax[i // 2, i % 2].dist = 10
+                dt_ax[i // 2, i % 2].elev = 20
+                dt_ax[i // 2, i % 2].invert_xaxis()
+                dt_ax[i // 2, i % 2].set_xticks(all_fz)
+                dt_ax[i // 2, i % 2].set_yticks(all_dist)
+                dt_ax[i // 2, i % 2].set(xlabel="File Size[MB]", ylabel="Distance [m]", zlabel="Download time [s]",
+                          title=f"{algo} Download Time")
 
         colors = ["yellow", "red", "blue", "black"]
-        cmap = ['spring', 'summer', 'autumn', 'winter']
+        thr_cmap = ['gist_heat' for i in range(4)]
+        dt_cmap = ['coolwarm' for i in range(4)]
         index = -1
+
+        min_thr = min(throughputs)
+        max_thr = max(throughputs)
+        min_dt = min(downloads_time)
+        max_dt = max(downloads_time)
+
         for specific_algo, folders in results.items():
                 index += 1
                 distances = []
@@ -87,23 +129,28 @@ def plot_algo(algo, results):
                 throughputs = [float(x) for x in throughput]
                 downloads_time = [float(x) for x in download_time]
 
-                thr_ax.set_ylim(max(distances), min(distances))
+                thr_ax[index // 2, index % 2].set_ylim(max(distances), min(distances))
+
+                thr_ax[index // 2, index % 2].set_title(f"{specific_algo} Throughput", pad=5)
+                dt_ax[index // 2, index % 2].set_title(f"{specific_algo} Download Time", pad=5)
 
                 distances = np.array(distances)
                 files_size = np.array(files_size)
                 throughputs = np.array(throughputs)
                 downloads_time = np.array(downloads_time)
    
-                if False: 
-                        thr_ax.plot_trisurf(files_size, distances, throughputs, cmap=cmap[index], antialiased=False, alpha=0.5, linewidth = 0.25, vmin = throughputs.min() * 0.5, vmax = throughputs.max() * 2.5)
-                        dt_ax.plot_trisurf(files_size, distances, downloads_time, cmap=cmap[index], antialiased=False, alpha=0.5, linewidth = 0.25, vmin = downloads_time.min() * 0.5, vmax = downloads_time.max() * 2.5)
+                if True: 
+                        thr_ax[index // 2, index % 2].plot_trisurf(files_size, distances, throughputs, cmap=thr_cmap[index], antialiased=False, alpha=0.5, linewidth = 0.25, vmin = min_thr * 0.1, vmax = max_thr * 2)
+                        dt_ax[index // 2, index % 2].plot_trisurf(files_size, distances, downloads_time, cmap=dt_cmap[index], antialiased=False, alpha=0.5, linewidth = 0.25, vmin = min_dt * 0.1, vmax = max_dt * 2)
                 else:
-                        thr_ax[0, index].plot_trisurf(files_size, distances, throughputs, color=colors[index], antialiased=False, alpha=0.5, linewidth = 0.25)
-                        dt_ax[0, index].plot_trisurf(files_size, distances, downloads_time, color=colors[index], antialiased=False, alpha=0.5, linewidth = 0.25)
-        thr_fig.tight_layout()
+                        thr_ax[index // 2, index % 2].plot_trisurf(files_size, distances, throughputs, color=colors[index], antialiased=False, alpha=0.5, linewidth = 0.25)
+                        dt_ax[index // 2, index % 2].plot_trisurf(files_size, distances, downloads_time, color=colors[index], antialiased=False, alpha=0.5, linewidth = 0.25)
+
+        thr_fig.tight_layout(rect=[0, 0.01, 0.98, 0.95])
+        dt_fig.tight_layout(rect=[0, 0.01, 0.98, 0.95])
+
         thr_fig.savefig(f'{algo}_throughputs.png')
         
-        dt_fig.tight_layout()
         dt_fig.savefig(f'{algo}_dtime.png')
 
                
